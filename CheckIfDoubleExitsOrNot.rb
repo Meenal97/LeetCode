0f2c52e57c95331818db543fb8972bdc082ce9d9
Meenal97 <meenalpreet94@gmail.com>
2020-06-13 17:07:29 +0530

    Not done

---
+++ CheckIfDoubleExitsOrNot.rb
@@ -0,0 +1,21 @@
+class Check
+  def Check_if_exist(arr)
+    array = []
+    arr.each_with_index do |num, i|
+      if arr.include?(num*2)
+        array << true
+        break
+      else 
+        array << false
+      end
+    end
+    array[-1]
+  end
+end
+arr = [10,2,5,3]
+c = Check.new
+puts c.Check_if_exist(arr)
+puts "************************************************"
+puts c.Check_if_exist([3,1,7,11])
+puts "***********************************************"
+puts c.Check_if_exist( [-2, 0, 10, -19, 4, 6, -8])
