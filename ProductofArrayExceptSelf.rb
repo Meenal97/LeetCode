0b776a7d4075cdd9b9341a507c420409f69df0c1
Meenal97 <meenalpreet94@gmail.com>
2020-06-17 22:30:26 +0530

    Here

--- ProductofArrayExceptSelf.rb
+++ ProductofArrayExceptSelf.rb
@@ -1,12 +1,20 @@
-class Product
-  def p nums
-    product = nums.inject(&:*)
+class Product 
+   def expect_self arr
+    product = arr.include?(0) ? (arr - [0]).inject(&:*) : arr.inject(&:*)
     without_self = []
-    nums.each do |num|
-      without_self << product / num
+    arr.each do |i|
+      if i == 0
+        without_self << product
+      else
+        without_self << product / i
+      end
     end
     without_self
-  end
+   end
 end
+arr = [1,3,4,5]
+arr2 = [2,0,3,5]
 p = Product.new
-puts p.p([1,2,3,4])
+puts p.expect_self(arr)
+puts "*****************************"
+puts p.expect_self(arr2)
