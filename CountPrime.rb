b7ff20dcbc0882fcdafd7c26df509059d7e8143d
Meenal97 <meenalpreet94@gmail.com>
2020-06-25 00:30:51 +0530

    Count of primes

--- CountPrime.rb
+++ CountPrime.rb
@@ -1,15 +1,16 @@
 class CountP
   def prime n
-    isPrime = true
-    for i in 2..n/2 
-      if n % i == 0
-       isPrime = false
-       break;
-      end
+    nonPrime = []
+    for j in 3...n
+        for i in 2...n/2
+          if j % i == 0 
+            nonPrime << j
+            break;
+          end
+        end
     end
-    isPrime
+    ((2..10).to_a - nonPrime).count
   end
 end
 c = CountP.new
 puts c.prime(10)
-puts c.prime(7)
