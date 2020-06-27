e753bc78b7341168e194cd6bd8c95576ec2c0468
Meenal97 <meenalpreet94@gmail.com>
2020-06-27 17:05:47 +0530

    majority

--- Majority.rb
+++ Majority.rb
@@ -1,9 +1,10 @@
 class Major
   def rep arr
-    arr. { |num| arr.count(num) > arr.size/2 }     
-    
+    freq = arr.inject(Hash.new(0)) { |h,v| h[v] +=1;h }
+    arr.max_by { |v| freq[v] }
   end
 end
 m = Major.new
 puts m.rep([2,2,1,1,1,2,2])
+puts "*****************************************"
 puts m.rep([3,2,3])
