3a496cdf2c00fd4aee75bbd70ff828310c369588
MeenalKaurLNG <meenalpreet.kaur@lngconsultancy.co.in>
2020-07-04 14:34:14 +0530

    Perfect Number

--- PerfectNumber.rb
+++ PerfectNumber.rb
@@ -1,14 +1,16 @@
 class Number
     def perfect_number num
-       number = []
-       (1..num/2).each do |n|
-          if num % n == 0
-            number << n
-          end
-       end
-       number.sum == num ? true : false
+      return false if 1 == num
+      sum = 1
+      i = 2
+      while i*i <= num
+          sum += i + num/i if 0 == num%i
+          i += 1
+      end
+      p sum
+      sum == num
     end
 end
 num = Number.new
 
-print num.perfect_number(28)
+print num.perfect_number(10000000)
